cedc6256e4cf053f4f4021221038c74a3db423fd
rakeshullengula56 <ramullengularakesh@gmail.com>
2025-01-12 19:49:15 +0530

    updated the repository and properties class and created some testcases in TestClass

--- src/main/java/com/ram/SpringJPATutorial/repositories/ProductRepository.java
+++ src/main/java/com/ram/SpringJPATutorial/repositories/ProductRepository.java
@@ -2,10 +2,19 @@ package com.ram.SpringJPATutorial.repositories;
 
 import com.ram.SpringJPATutorial.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 @Repository
-public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    @Query("SELECT p FROM ProductEntity p WHERE p.title = :title")
+    Optional<ProductEntity> findByTitle(@Param("title") String title);
+
 }
+
 
 
--- src/test/java/com/ram/SpringJPATutorial/SpringJpaTutorialApplicationTests.java
+++ src/test/java/com/ram/SpringJPATutorial/SpringJpaTutorialApplicationTests.java
@@ -1,18 +1,37 @@
 package com.ram.SpringJPATutorial;
 
-
-
+import com.ram.SpringJPATutorial.entities.ProductEntity;
+import com.ram.SpringJPATutorial.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class SpringJpaTutorialApplicationTests {
-//	@Autowired
-//    ProductRepository productRepository;
+	@Autowired
+	ProductRepository productRepository;
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void testRepo(){
+		ProductEntity productEntity= ProductEntity.builder()
+				.sku("tetley")
+				.title("tetley tea")
+				.price(BigDecimal.valueOf(80.99))
+				.quantity(12)
+				.build();
+		ProductEntity savedProductEntity=productRepository.save(productEntity);
+		System.out.println(savedProductEntity);
+	}
+	@Test
+	void getRepo(){
+		Optional<ProductEntity> products=productRepository.findByTitle("Almonds");
+		System.out.println(products);
 	}
 
 }
