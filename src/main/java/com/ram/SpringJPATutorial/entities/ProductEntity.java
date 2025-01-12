f8538ea45279b88d2066f4e11ea18a373320c148
rakeshullengula56 <ramullengularakesh@gmail.com>
2025-01-12 16:46:49 +0530

    Created entity class and used various annotations such @Entity,@Table,@Column etc...

---
+++ src/main/java/com/ram/SpringJPATutorial/entities/ProductEntity.java
@@ -0,0 +1,53 @@
+package com.ram.SpringJPATutorial.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "sku_unique", columnNames = {"sku"}),
+                @UniqueConstraint(name = "title_price_unique", columnNames = {"title_x", "price"})
+        },
+        indexes = {
+                @Index(name = "sku_index", columnList = "sku")
+        }
+)
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String sku;
+
+    @Column(name = "title_x", length = 100, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer quantity = 0;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
