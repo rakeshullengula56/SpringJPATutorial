3432b929420de773dbdd654bc87ecde417c34adc
rakeshullengula56 <ramullengularakesh@gmail.com>
2025-01-12 16:42:49 +0530

    Created Project using Lombok,Web,Data JPA,Mysql Driver Dependencies

---
+++ src/main/java/com/ram/SpringJPATutorial/SpringJpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.ram.SpringJPATutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ram/SpringJPATutorial/SpringJpaTutorialApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ram.SpringJPATutorial;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringJpaTutorialApplicationTests {
+//	@Autowired
+//    ProductRepository productRepository;
+	@Test
+	void contextLoads() {
+	}
+
+}
